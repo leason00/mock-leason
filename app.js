8a23c3401e3cc2f769be714f417efedd34bbac6c
leason00 <22647085@qq.com>
2017-11-18 17:45:19 +0800

    [A]添加跨域组件

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var cors = require('cors');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
